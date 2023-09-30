988f08dd2c66e63ad215226b9b25f12c1724fc00
mateus_ferraz.s@hotmail.com <mateus_ferraz,s@hotmail.com>
2023-09-30 11:23:40 -0300

    Realizado ajuste estrutura e ajuste Class Account

--- backend/src/FBank.Domain/Entities/Account.cs
+++ backend/src/FBank.Domain/Entities/Account.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FonteSaber.Infra.CrossCutting.Enuns;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,10 @@ namespace FBank.Domain.Entities
 {
     public  class Account : EntityBase
     {
-        public int ClientId { get; set; }
+        public Guid ClientId { get; set; }
         public Client Client { get; set; }
+
+        [EnumDataType(typeof(AccountStatusEnum))]
         public int IdStatus { get; set; }
         public Decimal Saldo { get; set; }
     }
