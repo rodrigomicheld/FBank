8aa0b341e84fa466427d0441f090f50fc5bf573d
Rodrigo Michel <rodrigo.micheld@gmail.com>
2023-10-01 17:22:46 -0300

    Criado testes da controller client

--- backend/tests/FBank.UnitTests/Presentation/ClientControllerTests.cs
+++ backend/tests/FBank.UnitTests/Presentation/ClientControllerTests.cs
@@ -2,7 +2,6 @@
 using FBank.Application.ViewMoldels;
 using FBank.Presentation.Controllers;
 using MediatR;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 
@@ -30,29 +29,17 @@ namespace FBank.UnitTests.Presentation
         }
 
         [Fact]
-        public void Shoud_return_bad_request_when_client_does_not_exist()
+        public void Shoud_return_bad_request_when_client_does_not_existAsync()
         {
             var id = new Guid();
             var result = _mockMediator.Setup(obj => obj.Send(It.IsAny<GetOneClientQuery>(), new CancellationToken())).Throws<Exception>();
 
             var response = _clientController.GetOneAsync(id).Result;
 
-            ActionResult<ClientViewModel> responseResult = response;
-            //Assert.IsType<BadRequest>(response);
+            var resultado = response?.Result;
 
-            //ActionResult responseResult = response.Result;
-
-            //Assert.Equal(400, responseResult.Result);
-            Assert.IsType<BadRequest>(responseResult);
-            Assert.Null(response.Value);
-
-            //  Assert.ThrowsAnyAsync<NullReferenceException>(() => _clientController.GetOneAsync(It.IsAny<Guid>()));
-
-            ////  Assert.Equal(404, )
-
-             
-            //  Assert.IsType<BadRequestResult>(_clientController.Response.StatusCode);
-
+            Assert.Null(response?.Value);
+            Assert.IsType<BadRequestObjectResult>(resultado);
         }
     }
 }
