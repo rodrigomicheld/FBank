cb85bee5fd481df4922c435e9c1cd8efe556a2da
Carlos Bernardes de Abreu Jr <carlos.bernardes@apinteractive.com.br>
2023-10-06 18:52:07 -0300

    Pegar connection

--- backend/src/FBank.Infrastructure/DependencyInjection.cs
+++ backend/src/FBank.Infrastructure/DependencyInjection.cs
@@ -21,7 +21,7 @@ namespace FBank.Infrastructure
 
         private static void AddSqlServer(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(configuration.GetConnectionString("ConnectionStrings:DefaultConnection")));
+            services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
         }
     }
 }
