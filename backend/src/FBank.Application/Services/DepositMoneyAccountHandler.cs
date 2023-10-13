019ff648a46904eacfab93a4ad83d4d7d4eb66be
Edilson <edilsoncfc@yahoo.com>
2023-10-12 22:24:54 -0300

    feature(EfetuarDeposito): Remoção de bibliotecas não utilizadas

--- backend/src/FBank.Application/Services/DepositMoneyAccountHandler.cs
+++ backend/src/FBank.Application/Services/DepositMoneyAccountHandler.cs
@@ -5,11 +5,6 @@ using FBank.Application.ViewMoldels;
 using FBank.Domain.Entities;
 using MediatR;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FBank.Application.Services
 {
